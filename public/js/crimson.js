15365e95ffcbd15849d80f4eabe7995f90c1734e
Howard Pang <howard.p@pervices.com>
2015-04-21 23:27:55 +0000

    Jquery with switches and tabs

--- public/js/crimson.js
+++ public/js/crimson.js
@@ -8,3 +8,25 @@ $(document).ready(function() {
    });
 });
 
+// Switch channel views
+// This function will load the current states of the channel onto the page
+$("#txchan_a,#txchan_b,#txchan_c,#txchan_d,#rxchan_a,#rxchan_b,#rxchan_c,#rxchan_d").click(function() {
+   $(this).parent().parent().children().removeClass('active');
+   $(this).parent().attr('class', 'active');
+
+   // load the channel state
+});
+
+// En/disable channel
+// This function will enable the channel
+$("#txchan_en,#rxchan_en").on('switchChange.bootstrapSwitch', function(event, state) {
+   // enable the channels
+});
+
+// En/disable DHCP
+// This function will enable the DHCP
+$("#dhcp_en").on('switchChange.bootstrapSwitch', function(event, state) {
+   $("#mgmt_ip"     ).prop('disabled', state);
+   $("#mgmt_gateway").prop('disabled', state);
+   $("#mgmt_netmask").prop('disabled', state);
+});
